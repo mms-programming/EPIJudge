33ee6f0ab6aeee3962a7e89dea8930b8d83e21ae
mms-programming <mms.programming@gmail.com>
2018-10-13 21:53:56 -0400

    Initial h_index solution

--- epi_judge_python/h_index.py
+++ epi_judge_python/h_index.py
@@ -2,9 +2,30 @@ from test_framework import generic_test
 
 
 def h_index(citations):
-    # TODO - you fill in here.
-    return 0
+    max_citations = get_max(citations)
+    for i in range(0, max_citations+1):
+        count = calculate_articles_with_citations(citations, i)
+        if count < i:
+            return i - 1
 
+    return i
+
+
+def get_max(citations):
+    max_citation = 0
+    for citation in citations:
+        if citation > max_citation:
+            max_citation = citation
+    return max_citation
+
+
+def calculate_articles_with_citations(citations, threshold):
+    count = 0
+    for citation in citations:
+        if citation >= threshold:
+            count += 1
+
+    return count
 
 if __name__ == '__main__':
     exit(generic_test.generic_test_main("h_index.py", 'h_index.tsv', h_index))
